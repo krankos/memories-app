5c752b4a077a02cf9d78c99cf4ff46fd232cd521
Khalil Bessaad <bessaadkhalil@gmail.com>
2022-04-17 20:09:59 +0100

    displaying posts using selector hook and redux setup

---
+++ src/reducers/posts.js
@@ -0,0 +1,14 @@
+import Posts from "../components/Posts/Posts";
+
+export default (posts = [], action) => {
+    switch (action.type) {
+        case "FETCH_ALL":
+            // return posts;
+            return action.payload;
+        case "CREATE":
+            return posts;
+
+        default:
+            return posts;
+    }
+};
