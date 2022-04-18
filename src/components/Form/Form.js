908823e21cea3a4ce2b90eef3632fb2fa2cd8130
Khalil Bessaad <bessaadkhalil@gmail.com>
2022-04-18 17:34:00 +0100

    finished form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
-
+import FileBase from "react-file-base64";
 import useStyles from "./styles";
 
 const Form = () => {
@@ -14,12 +14,13 @@ const Form = () => {
     tags: "",
     selectedFile: "",
   });
+  const clear = () => {};
   return (
     <Paper className={classes.paper}>
       <form
         autoComplete="off"
         noValidate
-        className={classes.form}
+        className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">Creating a Memory</Typography>
@@ -33,6 +34,60 @@ const Form = () => {
             setPostData({ ...postData, creator: e.target.value })
           }
         />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectedFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          fullWidth
+          onClick={clear}
+        >
+          Clear
+        </Button>
       </form>
     </Paper>
   );
